c6392e4b0f1828085a7223439970d6530509e6b6
ch2isk4kos <ch2isk4kos@gmail.com>
2022-01-30 14:12:18 -0500

    revise user id logs response

--- server.js
+++ server.js
@@ -348,8 +348,21 @@ app.get("/api/users/:_id/logs", (req, res) => {
   let user = users.find((u) => u._id === _id);
   console.log("user:", user);
 
-  if (user) return res.json(user);
-  else return res.status(400).send("User not found");
+  if (user) {
+    let logs = user.logs;
+    return res.json({
+      _id: user._id,
+      username: user.username,
+      count,
+      log: logs.map((log) => {
+        return {
+          description: log.description,
+          duration: log.duration,
+          date: log.date,
+        };
+      }),
+    });
+  } else return res.status(400).send("User not found");
 });
 
 app.get("/api/users/:id/logs", (req, res) => {
